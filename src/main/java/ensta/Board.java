53b9d0e8eb616424685bdbdf6215b9d49341f18c
Joyce-Tchamdjou <joyce-pascale.tchamdjou-mbotchack@ensta-paris.fr>
2021-02-16 09:54:27 +0100

    Exercice 1

---
+++ src/main/java/ensta/Board.java
@@ -0,0 +1,103 @@
+class Board{
+	/** Nom du Board */
+	private String nom;
+	/** Tableau 2D des navires*/
+	public char[][] navires;
+	/** Tableau 2D des frappes*/
+	public boolean[][] frappes;
+
+	/**
+	 *Constructeur valué 1
+	 *@param name nom du board
+	 *@param size taille du board
+	 */
+	public Board(String name, int size){
+		nom = name;
+		navires = new char[size][size];
+		frappes = new boolean[size][size];
+	}
+
+	/**
+	 *Constructeur valué 2
+	 *@param name nom du board
+	 */
+	public Board(String name){
+		nom = name;
+		navires = new char[10][10];
+		frappes = new boolean[10][10];
+	}
+
+	/**
+	 *getters
+	 */
+	public String get_nom(){
+		return nom;
+	}
+
+	/**
+	 *setters
+	 */
+	public void set_nom(String name){
+		nom = name;
+	}
+
+	/**
+	 *Fonction qui affiche les tableaux
+	 */
+	public void print(){
+		int size_navires = navires.length;
+		int size_frappes = frappes.length;
+		char[] lettres = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; 
+		
+		System.out.println("Navires :");
+		System.out.print(" "+"\t");
+
+		for(int i = 0; i < size_navires; i++){
+			System.out.print(lettres[i] + "\t");
+		}
+		System.out.print("\n");
+
+		for(int i = 1; i <= size_navires; i++){
+			System.out.print(i + "\t");
+			for(int j = 0; j < size_navires; j++){
+				System.out.print(navires[i-1][j] + "\t");
+			}
+			System.out.print("\n");
+		}
+
+		System.out.println();
+		System.out.println("Frappes :");
+		System.out.print(" "+"\t");
+
+		for(int i = 0; i < size_frappes; i++){
+			System.out.print(lettres[i] + "\t");
+		}
+		System.out.print("\n");
+
+		for(int i = 1; i <= size_frappes; i++){
+			System.out.print(i + "\t");
+			for(int j = 0; j < size_frappes; j++){
+				if(frappes[i-1][j] == true){
+					System.out.print("x" + "\t");
+				}else{
+					System.out.print("." + "\t");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+}
+
+/*public class TestBoard{
+	public static void main(String[] args){
+		Board test = new Board("Joyce");
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				test.navires[i][j] = '.';
+				test.frappes[i][j] = false;
+			}
+		}
+		test.print();
+	}
+
+}*/
